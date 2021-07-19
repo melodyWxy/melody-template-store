7a7e31d9e2e30a81c252a3e61a641f2969692a88
melodyls <17611203170@163.com>
2021-07-19 18:04:52 +0800

    feat: 新增套件开发模板

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 # melody-template-store
 
 集成了所有的模板，供给给@melody-core/leo脚手架使用。
+
+
+## melody-plugin-template
+
+用于开发melody-cli命令行套件的模板
+
+## 开发注意点
++ 开局先修改package.json文件
+    + name : @melody-core/xxx   必须以@melody-core开头！命名不要和其他套件重复，不然无法发布
+    + bin :  将“test” 修改为你自定义的命令。 不要和已有的全局命令重复哦。
+
++ 愉快的开发吧！
+    + 注册option => Program.option()
+    + 注册命令 => Program.commander()
+
++ 命令参数注意
+    + ccc \< arg \> 尖括号内定义必传参数！
+    + ccc \[arg\] 中括号内定义可选参数！  
--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+#!/usr/bin/env node
 
-console.log('这是模板1');
+const { Command } = require("commander");
+const program = new Command();
+class Main {
+   start() {
+    // 版本
+    program
+      .version(require("./package.json").version)
+      .option("-v, --version", "查看当前版本");
+
+    // 项目初始化命令
+    program.command("test <arg>")
+        .description('测试命令1')
+        .action(async (pk)=>{
+             // todosth
+            console.log('test1:', arg);
+        })
+
+    // 注册命令 test2
+    program.command("test2 [arg]")
+        .description( "测试命令2")
+        .action(async (arg)=>{
+            // todosth
+            console.log(arg);
+        })
+
+    // 解析环境参数，不要删除
+    program.parse(process.argv);
+  }
+}
+
+
+new Main().start();
